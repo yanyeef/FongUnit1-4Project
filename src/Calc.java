b43b9a3a27a9def366a828c3c6574f7dd7541c17
BT_3W2_03 <yanyeef@nycstudents.net>
2022-11-28 09:28:51 -0500

    changes

---
+++ src/Calc.java
@@ -0,0 +1,22 @@
+public class Calc {
+    private double first;
+    private double second;
+
+    //constructor class
+    public Calc(double one, double two) {
+        this.first = one;
+        this.second = two;
+    }
+
+    public double add(String expression){
+        String current = "";
+        double sum = 0;
+        for (int i =0; i < expression.length(); i++){
+            if(expression.indexOf(i) != -1){
+                sum +=
+            }
+        }
+    }
+
+
+}
---
+++ src/CalcRunner.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class CalcRunner {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Type in the first value: ");
+        String one = scan.nextLine();
+        System.out.print("Type out the mathematical sign (Ex: addition = +): ");
+        System.out.print("Type the second value: ");
+        String two = scan.nextLine();
+    }
+}
