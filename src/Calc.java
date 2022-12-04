5a50dc8a00ea5aabaa556a4f313568c5d1298400
yanyeef <yanyeef@nycstudents.net>
2022-12-04 17:01:33 -0500

    changes 2

--- src/Calc.java
+++ src/Calc.java
@@ -1,40 +1,41 @@
 
+/**
+ * The Calc class represents a Calculator with values and math sign
+ */
 public class Calc {
     private double first;
     private double second;
     private String expression;
+    private String sign;
     private double radian;
+    private int numOfValues;
 
     //constructor class
-    public Calc(double one, double two) {
+    public Calc(double one, String sign){
         this.first = one;
-        this.second = two;
+        this.sign = sign;
+    }
+// if user just types = it will return 0
+
+
+    public Calc(){
+        this.first =0;
+        this.sign = "+";
+        this.second = 0;
     }
 
-    public Calc(double one){
-        this.first = one;
-
-    }
-
-    public Calc(String expression){
+    public Calc(String expression, String sign){
         this.expression = expression;
+        this.sign = sign;
+    }
+    public void zero(){
+        int zero = 0;
+        System.out.println("=" + zero);
     }
 
-    public double add(){
-        return first + second;
-    }
 
-    public double subtract() {
-        return first -second;
-    }
 
-    public double multiply() {
-        return first* second;
-    }
 
-    public double divide() {
-        return (double) first/ second;
-    }
 
     public double radian(double first) {
         this.radian =Math.toRadians(first);
@@ -59,6 +60,142 @@ public class Calc {
 
     public double pow() {
         return Math.pow(first, second);
+    }
+
+    public double oneVal() {
+        double result =0;
+        if (sign.equals("sqrt")){
+            result = sqrt();
+        } else if (sign.equals("sin")){
+            result = sin(radian(first));
+        } else if (sign.equals("cos")){
+            result = cos(radian(first));
+        } else if(sign.equals("tan")){
+            result = tan(radian(first));
+        }
+        return result;
+    }
+
+    public double twoVal(int numOfValues, String sign) {
+        double result = 0;
+        if (sign.equals("+")) {
+            result = expressionAdd(numOfValues, sign);
+        } else if (sign.equals("-")) {
+            result = sub(numOfValues, sign);
+        } else if (sign.equals("*")) {
+            result = mult(numOfValues, sign);
+        } else if (sign.equals("/")) {
+            result = div(numOfValues,sign);
+        } else if (sign.equals("^")) {
+            result = pow();
+        }
+        return result;
+    }
+
+    public String toString1() {
+        if(!sign.equals("!")) {
+            return sign + " " + first + " = " + "\n" + oneVal();
+        } else {
+            return printFactoral();
+        }
+    }
+
+    public String toString2(int numOfValues){
+        String current = expression;
+        String end = "";
+        while (numOfValues > 0) {
+            double value = Double.parseDouble(current.substring(0, current.indexOf(" ")));
+            if (current.length() > 3 && current.contains(sign)) {
+                current = current.substring(current.indexOf(" ") + 3);
+            }
+            end += value + " " + sign;
+            numOfValues--;
+        }
+        return end;
+    }
+
+    public int factoral(){
+        double result = 1;
+        while( first >1) {
+            result = result * first;
+            first--;
+        }
+        return (int)result;
+    }
+
+    public String printFactoral() {
+        return (int)first + sign  + " = " + "\n" + factoral();
+    }
+
+    public double expressionAdd(int numOfVal, String sign){
+
+        String current = expression.substring(0, expression.length()-1);
+        double num =0;
+
+        if(sign.equals("+")) {
+            while (numOfVal > 0) {
+                double value = Double.parseDouble(current.substring(0, current.indexOf(" ")));
+                if (current.length() > 3 && current.contains("+")) {
+                    current = current.substring(current.indexOf(" ") + 3);
+                }
+                num += value;
+                numOfVal--;
+            }
+        }
+        return num;
+    }
+
+    public double sub(int numOfVal, String sign){
+        String current = expression.substring(0, expression.length()-1);
+        double num =Double.parseDouble(current.substring(0, current.indexOf(" ")));
+        current = current.substring(current.indexOf(" ") + 3);
+
+        if(sign.equals("-")) {
+            while (numOfVal > 1) {
+                double value = Double.parseDouble(current.substring(0, current.indexOf(" ")));
+                if (current.length() > 3 && current.contains("-")) {
+                    current = current.substring(current.indexOf(" ") + 3);
+                }
+                num -= value;
+                numOfVal--;
+            }
+        }
+        return num;
+    }
+
+    public double mult(int numOfVal, String sign){
+        String current = expression.substring(0, expression.length()-1);
+        double num =1;
+
+        if(sign.equals("*")) {
+            while (numOfVal > 0) {
+                double value = Double.parseDouble(current.substring(0, current.indexOf(" ")));
+                if (current.length() > 3 && current.contains("*")) {
+                    current = current.substring(current.indexOf(" ") + 3);
+                }
+                num = num* value;
+                numOfVal--;
+            }
+        }
+        return num;
+    }
+
+    public double div(int numOfVal, String sign){
+        String current = expression.substring(0, expression.length()-1);
+        double num =Double.parseDouble(current.substring(0, current.indexOf(" ")));
+        current = current.substring(current.indexOf(" ") + 3);
+
+        if(sign.equals("/")) {
+            while (numOfVal > 1) {
+                double value = Double.parseDouble(current.substring(0, current.indexOf(" ")));
+                if (current.length() > 3 && current.contains("/")) {
+                    current = current.substring(current.indexOf(" ") + 3);
+                }
+                num /= value;
+                numOfVal--;
+            }
+        }
+        return num;
     }
 
 
--- src/CalcRunner.java
+++ src/CalcRunner.java
@@ -1,70 +1,46 @@
+
 import java.util.Scanner;
+
+
 public class CalcRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
         String expression = "";
+        System.out.println("If you want sin, cos, tan, !, or sqrt, type out the sign on the first line, then type out the value");
         System.out.print("Type in a value or sign: ");
         String input = scan.nextLine();
-        expression+= input + " ";
-        while(!(input.equals("="))){
+        expression += input + " ";
+        while (!(input.contains("="))) {
             System.out.print("Type in a value or sign: ");
             input = scan.nextLine();
-            expression+= input + " ";
+            expression += input + " ";
         }
 
-        if(input.equals("=")) {
-            System.out.println(expression);
-            System.out.println("How much values did you input: ");
+        if (input.contains("=")) {
+            System.out.print("How much values did you input: ");
             int numOfValues = scan.nextInt();
-            double result = 0;
 
-            if(numOfValues==1){
-                String single = expression.substring(expression.indexOf(" ") +1, expression.indexOf("=") - 1);
+            if(numOfValues ==0){
+                Calc noValue = new Calc();
+                noValue.zero();
+            }
+            else if (numOfValues == 1) {
+                String single = expression.substring(expression.indexOf(" ") + 1, expression.indexOf("=") - 1);
                 double s = Double.parseDouble(single);
                 String math = expression.substring(0, expression.indexOf(" "));
+                Calc oneValue = new Calc(s, math);
+                System.out.println(oneValue.toString1());
 
-                Calc oneValue = new Calc(s);
-                if(math.equals("sqrt")){
-                    result = oneValue.sqrt();
-                    System.out.println(expression + result);
-                } else if (math.equals("sin")){
-                    result = oneValue.sin(oneValue.radian(s));
-                    System.out.println(expression + result);
-                } else if (math.equals("cos")){
-                    result = oneValue.cos(oneValue.radian(s));
-                    System.out.println(expression + result);
-                } else if( math.equals("tan")){
-                    result = oneValue.tan(oneValue.radian(s));
-                    System.out.println(expression + result);
-                }
+            } else {
+                String signEX = expression.substring(expression.indexOf(" ") + 1, expression.indexOf(" ") + 2);
+                Calc express = new Calc(expression, signEX);
+                System.out.println(express.twoVal(numOfValues, signEX));
+                System.out.println(express.toString2(numOfValues));
             }
-            else if(numOfValues ==2) {
 
-                String firstValue = expression.substring(0, expression.indexOf(" "));
-                double fV = Double.parseDouble(firstValue);
-                String secValue = expression.substring(expression.indexOf(" ") + 3, expression.indexOf("=") - 1);
-                double sV = Double.parseDouble(secValue);
-                String sign = expression.substring(expression.indexOf(" ") + 1, expression.indexOf(" ") + 2);
 
-                Calc twoNumbers = new Calc(fV, sV);
-                if (sign.equals("+")) {
-                    result = twoNumbers.add();
-                    System.out.println(expression + result);
-                } else if (sign.equals("-")) {
-                    result = twoNumbers.subtract();
-                    System.out.println(expression + result);
-                } else if (sign.equals("*")) {
-                    result = twoNumbers.multiply();
-                    System.out.println(expression + result);
-                } else if (sign.equals("/")) {
-                    result = twoNumbers.divide();
-                    System.out.println(expression + result);
-                } else if (sign.equals("^")) {
-                    result = twoNumbers.pow();
-                    System.out.println(expression + result);
-                }
-            }
         }
 
     }
+
 }
